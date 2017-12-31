61e8d2d9c93b5c54c66bd07962dd9a000b59db3e
Mathieu Bouchard <mbouchar@gmail.com>
2017-12-31 00:22:59 -0500

    Implémentation parallèle au lieu d'utiliser des tâches asynchrones Paramétrisation du DNS du serveur à utiliser

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,213 +9,230 @@ using System.Runtime.Serialization.Json;
 using ConsoleApp.Models;
 using System.IO;
 using Newtonsoft.Json;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static private HttpClient client = null;
+        private static HttpClient client = null;
+
+        private static string DEFAULT_HOSTNAME = "localhost";
 
         /* 
-         * set ASPNETCORE_ENVIRONMENT=Production
+         * Génération du certificat pour Kestrel:
+         *   openssl pkcs12 -inkey localhost.pem -in localhost.csr -export -out localhost.pfx
+         *
          * dotnet build -c Release
-         * dotnet run -c Release --no-build --no-restore
-         * 
+         * Windows:
+         *   set ASPNETCORE_ENVIRONMENT=Production
+         *   dotnet run -c Release --no-build --no-restore
+         * Autre:
+         *   ASPNETCORE_ENVIRONMENT=Production dotnet run -c Release --no-build --no-restore
          */
+
         static void Main(string[] args)
         {
-            // SOAP Client
-            ServiceReference.ServiceClient soapClient = new ServiceReference.ServiceClient(ServiceReference.ServiceClient.EndpointConfiguration.ServiceWithAnonymousAuthentication);
+            string hostname = DEFAULT_HOSTNAME;
+            if (args.Length > 0)
+                hostname = args[0];
+
+            string restReverseProxyHttpUrl = "http://" + hostname + "/RestService";
+            string restReverseProxyHttpsUrl = "https://" + hostname + "/RestService";
+            string restKestrelHttpUrl = "http://" + hostname + ":5000";
+            string restKestrelHttpsUrl = "https://" + hostname + ":5001";
+
+            bool testWCF = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            ServiceReference.ServiceClient soapClient = null;
+
+            if (testWCF)
+            {
+                // SOAP Client
+                soapClient = new ServiceReference.ServiceClient(ServiceReference.ServiceClient.EndpointConfiguration.ServiceWithAnonymousAuthentication);
 //            ServiceReference.ServiceClient soapClientHttps = new ServiceReference.ServiceClient(
 //                ServiceReference.ServiceClient.EndpointConfiguration.ServiceWithAnonymousAuthentication,
 //                "https://localhost/WcfService/Service.svc");
+            }
 
             // REST Client
             var filter = new HttpClientHandler();
             filter.ServerCertificateCustomValidationCallback = (request, cert, chain, errors) => { return true; };
             client = new HttpClient(filter);
 
-            int NUM_ITERATIONS = 1;
-            int CONCURRENT_REQUESTS = 1;
+            int NUM_ITERATIONS = 0;
+            int CONCURRENT_REQUESTS = 0;
+
+            /*
+             * Réveiller les services
+             */
 
             NUM_ITERATIONS = 500;
-            CONCURRENT_REQUESTS = 1;
-            // Réveiller les services
-            testSOAP(soapClient, NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
-   //         testSOAP(soapClientHttps, NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
-            testRestLargeData("http://localhost/RestService", NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
-            testRestLargeData("https://localhost/RestService", NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
-            testRestLargeData("http://localhost:5000", NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
-            testRestLargeData("https://localhost:5001", NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
+            CONCURRENT_REQUESTS = 10;
+            if (testWCF)
+            {
+                testSOAP(soapClient, NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
+//            testSOAP(soapClientHttps, NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
+            }
+            testRest(restReverseProxyHttpUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
+            testRestLargeData(restReverseProxyHttpUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
+            testRestLargeData(restReverseProxyHttpsUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
+            testRestLargeData(restKestrelHttpUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
+            testRestLargeData(restKestrelHttpsUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, null);
+            Thread.Sleep(5000);
 
-            /* Données simples
+            /*
+             * Données simples
              */
 
             Console.WriteLine("Données simples, Authentification Anonyme");
             NUM_ITERATIONS = 100000;
             CONCURRENT_REQUESTS = 100;
             Console.WriteLine("Nombre d'itérations: " + NUM_ITERATIONS + ", Requêtes concurrentes: " + CONCURRENT_REQUESTS);
+            if (testWCF)
+            {
+                testSOAP(soapClient, NUM_ITERATIONS, CONCURRENT_REQUESTS, "SOAP (HTTP)");
+                Thread.Sleep(5000);
+//            testSOAP(soapClientHttps, NUM_ITERATIONS, CONCURRENT_REQUESTS, "SOAP (HTTPS)");
+//            Thread.Sleep(5000);
+            }
+            testRest(restReverseProxyHttpUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTP, Reverse Proxy)");
             Thread.Sleep(5000);
-            // Test réel
-            testSOAP(soapClient, NUM_ITERATIONS, CONCURRENT_REQUESTS, "SOAP (HTTP)");
-     //       testSOAP(soapClientHttps, NUM_ITERATIONS, CONCURRENT_REQUESTS, "SOAP (HTTPS)");
+            testRest(restReverseProxyHttpsUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTPS, Reverse Proxy)");
             Thread.Sleep(5000);
-            testRest("http://localhost/RestService", NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTP, Reverse Proxy)");
+            testRest(restKestrelHttpUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTP, Kestrel)");
             Thread.Sleep(5000);
-            testRest("https://localhost/RestService", NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTPS, Reverse Proxy)");
-            Thread.Sleep(5000);
-            testRest("http://localhost:5000", NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTP, Kestrel)");
-            Thread.Sleep(5000);
-            testRest("https://localhost:5001", NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTPS, Kestrel)");
+            testRest(restKestrelHttpsUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTPS, Kestrel)");
             Thread.Sleep(5000);
 
-            /* Larges données (20 Ko)
+            /* 
+             * Larges données (20 Ko)
              */
+
             Console.WriteLine("Larges données, Authentification Anonyme");
             NUM_ITERATIONS = 5000;
             CONCURRENT_REQUESTS = 25;
             Console.WriteLine("Nombre d'itérations: " + NUM_ITERATIONS + ", Requêtes concurrentes: " + CONCURRENT_REQUESTS);
-            testSOAPLargeData(soapClient, NUM_ITERATIONS, CONCURRENT_REQUESTS, "SOAP (Large Data, HTTP)");
-     //       testSOAPLargeData(soapClientHttps, NUM_ITERATIONS, CONCURRENT_REQUESTS, "SOAP (Large Data, HTTPS)");
+            if (testWCF)
+            {
+                testSOAPLargeData(soapClient, NUM_ITERATIONS, CONCURRENT_REQUESTS, "SOAP (Large Data, HTTP)");
+                Thread.Sleep(5000);
+//            testSOAPLargeData(soapClientHttps, NUM_ITERATIONS, CONCURRENT_REQUESTS, "SOAP (Large Data, HTTPS)");
+//            Thread.Sleep(5000);
+            }
+            testRestLargeData(restReverseProxyHttpsUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTP, Large Data, Reverse Proxy)");
             Thread.Sleep(5000);
-            testRestLargeData("http://localhost/RestService", NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTP, Large Data, Reverse Proxy)");
+            testRestLargeData(restReverseProxyHttpsUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTPS, Large Data, Reverse Proxy)");
             Thread.Sleep(5000);
-            testRestLargeData("https://localhost/RestService", NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTPS, Large Data, Reverse Proxy)");
+            testRestLargeData(restKestrelHttpUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTP, Large Data, Kestrel)");
             Thread.Sleep(5000);
-            testRestLargeData("http://localhost:5000", NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTP, Large Data, Kestrel)");
-            Thread.Sleep(5000);
-            testRestLargeData("https://localhost:5001", NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTPS, Large Data, Kestrel)");
+            testRestLargeData(restKestrelHttpsUrl, NUM_ITERATIONS, CONCURRENT_REQUESTS, "REST .Net Core 2.0 (HTTPS, Large Data, Kestrel)");
 
             client.Dispose();
             Console.ReadLine();
         }
 
-        static void testSOAP(ServiceReference.ServiceClient client, int num_iterations, int concurrent_requests, string identifier)
+        private static void testSOAP(ServiceReference.ServiceClient client, int num_iterations, int concurrent_requests, string identifier)
         {
             var watch = Stopwatch.StartNew();
-            var tasks = new Task<string>[concurrent_requests];
-            var i = 0;
-            var n = 0;
-            var pos = -1;
-            while (i < num_iterations)
+
+            var requests_per_thread = num_iterations / concurrent_requests;
+            var result = Parallel.For(0, concurrent_requests, (i, state) => {
+                var task = client.GetDataAsync("ping");
+                task.Wait();
+                var value = task.Result as string;
+                task.Dispose();
+            });
+            if (!result.IsCompleted)
             {
-                if (n >= concurrent_requests)
-                {
-                    pos = Task.WaitAny(tasks);
-                    var value = tasks[pos].Result as string;
-                    tasks[pos].Dispose();
-                    tasks[pos] = null;
-                    n--;
-                }
-                else
-                    pos++;
-
-                tasks[pos] = client.GetDataAsync("ping");
-                i++;
-                n++;
+                throw new Exception("Parallel execution didn't finish correctly");
             }
-            Task.WaitAll(tasks);
-            watch.Stop();
 
+            watch.Stop();
             if (identifier != null)
                 Console.WriteLine(identifier + ": " + watch.ElapsedMilliseconds);
         }
 
-        static void testSOAPLargeData(ServiceReference.ServiceClient client, int num_iterations, int concurrent_requests, string identifier)
+        private static void testSOAPLargeData(ServiceReference.ServiceClient client, int num_iterations, int concurrent_requests, string identifier)
         {
             var watch = Stopwatch.StartNew();
-            var tasks = new Task<ServiceReference.LargeDataStructures>[concurrent_requests];
-            var i = 0;
-            var n = 0;
-            var pos = -1;
-            while (i < num_iterations)
+
+            var requests_per_thread = num_iterations / concurrent_requests;
+            var result = Parallel.For(0, concurrent_requests, (i, state) => {
+                var task = client.GetLargeDataAsync();
+                task.Wait();
+                var data = task.Result as ServiceReference.LargeDataStructures;
+                task.Dispose();
+            });
+            if (!result.IsCompleted)
             {
-                if (n >= concurrent_requests)
-                {
-                    pos = Task.WaitAny(tasks);
-                    var list = tasks[pos].Result as ServiceReference.LargeDataStructures;
-                    tasks[pos].Dispose();
-                    tasks[pos] = null;
-                    n--;
-                }
-                else
-                    pos++;
-
-                tasks[pos] = client.GetLargeDataAsync();
-                i++;
-                n++;
+                throw new Exception("Parallel execution didn't finish correctly");
             }
-            Task.WaitAll(tasks);
-            watch.Stop();
 
+            watch.Stop();
             if (identifier != null)
                 Console.WriteLine(identifier + ": " + watch.ElapsedMilliseconds);
         }
 
-        static void testRest(string baseUrl, int num_iterations, int concurrent_requests, string identifier)
+        private static void testRest(string baseUrl, int num_iterations, int concurrent_requests, string identifier)
         {
             var watch = Stopwatch.StartNew();
-            var tasks = new Task<string>[concurrent_requests];
-            var i = 0;
-            var n = 0;
-            var pos = -1;
-            while (i < num_iterations)
-            {
-                if (n >= concurrent_requests)
-                {
-                    pos = Task.WaitAny(tasks);
-                    var value = tasks[pos].Result as string;
-                    tasks[pos].Dispose();
-                    tasks[pos] = null;
-                    n--;
-                }
-                else
-                    pos++;
 
-                tasks[pos] = client.GetStringAsync(baseUrl + "/api/values/");
-                i++;
-                n++;
+            var requests_per_thread = num_iterations / concurrent_requests;
+            var result = Parallel.For(0, concurrent_requests, (i, state) => {
+                for (var j = 0; j < requests_per_thread; j++)
+                {
+                    var task = client.GetStringAsync(baseUrl + "/api/values/");
+                    task.Wait();
+                    var data = task.Result;
+                    task.Dispose();
+                }
+            });
+            if (!result.IsCompleted)
+            {
+                throw new Exception("Parallel execution didn't finish correctly");
             }
-            Task.WaitAll(tasks);
+
             watch.Stop();
-            Console.WriteLine(identifier + ": " + watch.ElapsedMilliseconds);
+            if (identifier != null)
+                Console.WriteLine(identifier + ": " + watch.ElapsedMilliseconds);
         }
 
-        static void testRestLargeData(string baseUrl, int num_iterations, int concurrent_requests, string identifier)
+        private static List<LargeDataStructure> parseJson(Stream data)
         {
+            List<LargeDataStructure> list = null;
             var serializer = new JsonSerializer();
 
-            var watch = Stopwatch.StartNew();
-            var tasks = new Task<Stream>[concurrent_requests];
-            var i = 0;
-            var n = 0;
-            var pos = -1;
-            while (i < num_iterations)
+            using (var sr = new StreamReader(data))
             {
-                if (n >= concurrent_requests)
+                using (var jsonTextReader = new JsonTextReader(sr))
                 {
-                    pos = Task.WaitAny(tasks);
-                    using (var sr = new StreamReader(tasks[pos].Result))
-                    {
-                        using (var jsonTextReader = new JsonTextReader(sr))
-                        {
-                            var list = serializer.Deserialize<List<LargeDataStructure>>(jsonTextReader);
-                        }
-                    }
-                    tasks[pos].Dispose();
-                    tasks[pos] = null;
-                    n--;
+                    list = serializer.Deserialize<List<LargeDataStructure>>(jsonTextReader);
                 }
-                else
-                    pos++;
-
-                tasks[pos] = client.GetStreamAsync(baseUrl + "/api/largevalues/");
-                i++;
-                n++;
             }
-            Task.WaitAll(tasks);
-            watch.Stop();
 
+            return list;
+        }
+
+        private static void testRestLargeData(string baseUrl, int num_iterations, int concurrent_requests, string identifier)
+        {
+            var watch = Stopwatch.StartNew();
+
+            var requests_per_thread = num_iterations / concurrent_requests;
+            var result = Parallel.For(0, concurrent_requests, (i, state) => {
+                for (var j = 0; j < requests_per_thread; j++)
+                {
+                    var task = client.GetStreamAsync(baseUrl + "/api/largevalues/");
+                    task.Wait();
+                    var data = parseJson(task.Result);
+                    task.Dispose();
+                }
+            });
+            if (!result.IsCompleted)
+            {
+                throw new Exception("Parallel execution didn't finish correctly");
+            }
+
+            watch.Stop();
             if (identifier != null)
                 Console.WriteLine(identifier + ": " + watch.ElapsedMilliseconds);
         }
